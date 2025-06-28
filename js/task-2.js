3b17b8d551ba9624ea7e300434ee3e88077d4627
ILLYA SMYK <smykillya@gmail.com>
2025-06-28 23:01:55 +0200

    4

--- js/task-2.js
+++ js/task-2.js
@@ -1,22 +1,24 @@
 function calcAverageCalories(days) {
-    const numbers = [];
+  if (days.length === 0) {
+    return "0";
+  }
+  const caloriesArr = [];
 
-    for (const day of days) {
-        numbers.push(day.calories);
-    }
-    
-    let total = 0;
-    for (const number of numbers) {
-        total += number;
-    }
-    const average = Math.round (total / numbers.length);
-    if (numbers.length === 0) { return "No data for this week"; }
-    
-    return average === 0 ? "No data for this week" : average;
+  for (const day of days) {
+    caloriesArr.push(day.calories);
+  }
+
+  let totalCalories = 0;
+  for (const calorie of caloriesArr) {
+    totalCalories += calorie;
+  }
+  const average = totalCalories / caloriesArr.length;
+
+  return average === 0 ? "0" : average;
 }
 
 console.log(
-calcAverageCalories([
+  calcAverageCalories([
     { day: "monday", calories: 3010 },
     { day: "tuesday", calories: 3200 },
     { day: "wednesday", calories: 3120 },
@@ -24,11 +26,11 @@ calcAverageCalories([
     { day: "friday", calories: 3450 },
     { day: "saturday", calories: 3280 },
     { day: "sunday", calories: 3300 },
-])
+  ])
 ); // 3180
 
 console.log(
-calcAverageCalories([
+  calcAverageCalories([
     { day: "monday", calories: 2040 },
     { day: "tuesday", calories: 2270 },
     { day: "wednesday", calories: 2420 },
@@ -36,6 +38,5 @@ calcAverageCalories([
     { day: "friday", calories: 2370 },
     { day: "saturday", calories: 2280 },
     { day: "sunday", calories: 2610 },
-])
+  ])
 ); // 2270
-
